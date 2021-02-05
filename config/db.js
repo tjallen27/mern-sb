5066f4652c0794b043d2e6a3a32d5b38e9abfa76
tjallen27 <tjallen91@hotmail.co.uk>
2021-02-05 22:00:14 +0000

    set mongo env vars.

--- config/db.js
+++ config/db.js
@@ -1,25 +1,22 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
-
     // Connect to database
-    await mongoose.connect(db, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useCreateIndex: true,
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useFindAndModify: false
+      useFindAndModify: false,
     });
 
     console.log('MongoDB Connected...');
   } catch (error) {
-
     // Exit process
-    console.error(err.message);
+    console.error(error.message);
     process.exit(1);
   }
-}
+};
 
 module.exports = connectDB;
